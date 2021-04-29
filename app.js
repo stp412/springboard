91d8db3871f9be9dea3b65a359717792d57c4277
stp412 <75588523+stp412@users.noreply.github.com>
2021-04-29 10:25:41 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+
+function countDown(n) {
+    let counter = 1;
+    let callback = function () {
+        if(n-counter > 0) {
+            console.log(n - counter);
+            counter++;
+        } else {
+            console.log("DONE!");
+            clearInterval(intervalId);
+        }
+    }
+    let intervalId = setInterval(callback, 1000);
+}
+
+function randomGame () {
+    let number = 0;
+    let counter = 1;
+    let callback = function () {
+        number = Math.random();
+        console.log("Number: " + number);
+        if (number > 0.75) {
+            console.log("Counter: " + counter);
+            clearInterval(intervalId);
+        } else {
+            counter++;
+        }
+    }
+    let intervalId = setInterval(callback, 1000);
+}
+
+randomGame();
+//countDown(11);
